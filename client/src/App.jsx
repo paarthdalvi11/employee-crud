0991d75bd36e6daab93c033a099d24a651ea76f0
paarthdalvi11 <paarthdalvi1011@gmail.com>
2024-12-02 01:14:15 +0530

    layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,16 +7,12 @@ import Home from './components/Home/Home';
 import Insert from './components/Insert/Insert';
 import Update from './components/Update/Update';
 import Delete from './components/Delete/Delete';
-import Card from './components/Card/Card';
-import Table from './components/Table/Table';
 
 function App() {
 
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/card' element={<Card />} />
-      <Route path='/table' element={<Table />} />
       <Route path='/insert' element={<Insert />} />
       <Route path='/update/:id' element={<Update />} />
       <Route path='/delete/:id' element={<Delete />} />
